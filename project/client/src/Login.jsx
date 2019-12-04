6b11f3454f542ff1af346ba64da22e6e954e2fca
Dani <tdanni95@gmail.com>
2019-12-04 12:44:43 +0100

    bcrypt changed to bcriptjs (server)

--- project/client/src/Login.jsx
+++ project/client/src/Login.jsx
@@ -1,163 +1,138 @@
-import React from 'react'
-import axios from 'axios'
-import './sass/Login.sass'
-import { getFromStorage, setInStorage } from './utils/storage'
+import React from 'react';
+import axios from 'axios';
+import './sass/Login.sass';
+import { getFromStorage, setInStorage } from './utils/storage';
 
+class Login extends React.Component {
+	constructor(props) {
+		super(props);
 
-class Login extends React.Component{
-  
-  constructor(props)
-  {
-    super(props)
+		this.loginOrRegister = this.loginOrRegister.bind(this);
+		this.onChangeUsername = this.onChangeUsername.bind(this);
+		this.onChangePassword = this.onChangePassword.bind(this);
+		this.onChangeInfo = this.onChangeInfo.bind(this);
+		this.onChangeToken = this.onChangeToken.bind(this);
 
-    this.loginOrRegister = this.loginOrRegister.bind(this)
-    this.onChangeUsername = this.onChangeUsername.bind(this)
-    this.onChangePassword = this.onChangePassword.bind(this)
-    this.onChangeInfo = this.onChangeInfo.bind(this)
-    this.onChangeToken = this.onChangeToken.bind(this)
+		this.state = {
+			username: '',
+			password: '',
+			infoMessage: '',
+			token: ''
+		};
+	}
 
-    this.state = {
-      username : '',
-      password : '',
-      infoMessage : '',
-      token : ''
-    }
+	componentDidMount() {}
+	onChangeInfo(msg) {
+		this.setState({
+			infoMessage: ''
+		});
+		this.setState({
+			infoMessage: (
+				<p id="info">
+					Info: <span>{msg}</span>
+				</p>
+			)
+		});
+	}
+	onChangeToken(e) {
+		this.setState({
+			token: e.target.value
+		});
+	}
+	onChangeUsername(e) {
+		this.setState({
+			username: e.target.value
+		});
+	}
+	onChangePassword(e) {
+		this.setState({
+			password: e.target.value
+		});
+	}
+	loginOrRegister(e) {
+		e.preventDefault();
 
-  }
+		if (this.state.username.length < 5) {
+			this.onChangeInfo("Username length can't be lower than five!");
+		} else if (this.state.password.length < 1) {
+			this.onChangeInfo('Password empty!');
+		} else {
+			let User = {
+				username: this.state.username,
+				password: this.state.password
+			};
+			axios.post('http://localhost:5000/users/', User).then((response) => {
+				if (response.data.success) {
+					if (response.data.exitCode === 1) {
+						User = {
+							username: this.state.username,
+							password: this.state.password
+						};
 
-  componentDidMount() 
-  {
+						axios.post('http://localhost:5000/users/', User).then((res) => {
+							if (res.data.exitCode === 0) {
+								this.setState({
+									infoMessage: res.data.message,
+									username: res.data.username,
+									token: res.data.token
+								});
 
-  }
-  onChangeInfo(msg)
-  {
-    this.setState({
-      infoMessage : ''
-    })
-    this.setState({
-      infoMessage : <p id="info">Info: <span>{msg}</span></p>
-    })
-  }
-  onChangeToken(e){
-    this.setState({
-      token : e.target.value
-    })
-  }
-  onChangeUsername(e)
-  {
-    this.setState({
-      username : e.target.value
-    })
-  }
-  onChangePassword(e)
-  {
-    this.setState({
-      password : e.target.value
-    })
-  }
-  loginOrRegister(e)
-  {
-    e.preventDefault()
-    
-    if(this.state.username.length < 5)
-    {
-      this.onChangeInfo("Username length can't be lower than five!")
-      
-    }
-    
-    else if(this.state.password.length < 1)
-    {
-      this.onChangeInfo("Password empty!")
-    }
-    else{
+								setInStorage('afp_falu', { token: res.data.token, username: res.data.username });
+								window.location = '/village';
+							} else {
+								this.setState({
+									infoMessage: response.data.message
+								});
+							}
+						});
+					} else if (response.data.exitCode === 0) {
+						this.setState({
+							infoMessage: response.data.message,
+							username: response.data.username,
+							token: response.data.token
+						});
 
-      let User = {
-        username : this.state.username,
-        password : this.state.password
-      }
-      axios.post("http://localhost:5000/users/", User)
-      .then(response => {
-        
-        if(response.data.success)
-        {
-          
-          if(response.data.exitCode === 1) 
-          {
-            
-            User = {
-              username : this.state.username,
-              password : this.state.password
-            }
-            
-            axios.post("http://localhost:5000/users/", User)
-              .then(res => {
-               
-                if(res.data.exitCode === 0)
-                {
-                 
-                  this.setState({
-                    infoMessage : res.data.message,
-                    username : res.data.username,
-                    token : res.data.token
-                  })
-                 
-                  setInStorage('afp_falu', {token : res.data.token, username : res.data.username})
-                  window.location = "/village"
-                }
-                else{
-                 
-                  this.setState({
-                    infoMessage : response.data.message,
-                  })
-                }
-            
-            })
-          }
-          else if(response.data.exitCode === 0)
-          {
-          
-            this.setState({
-              infoMessage : response.data.message,
-              username : response.data.username,
-              token : response.data.token
-            })
-           
-            setInStorage('afp_falu', {token : response.data.token, username : response.data.username})
-          }
-          
-           
+						setInStorage('afp_falu', { token: response.data.token, username: response.data.username });
+						window.location = '/village';
+					}
+				} else {
+					this.setState({
+						infoMessage: response.data.message
+					});
+				}
+			});
+		}
+	}
 
-        }
-        else
-        {
-          this.setState({
-            infoMessage : response.data.message
-          })
-        }
-        
-      })
-    }
-    
-  }
-
-  render()
-  {
-    return (
-      <div>
-          <aside>
-            {this.state.infoMessage}
-            <form onSubmit={this.loginOrRegister}>  
-                <input required value={this.state.username} onChange={this.onChangeUsername} type="text" name="username" placeholder="username" />
-                <input required value={this.state.password} onChange={this.onChangePassword} type="password" name="password" placeholder="password" />
-                <input type="submit" value="Play" />
-            </form>
-          </aside>
-          <section id="background">
-            
-          </section>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<aside>
+					{this.state.infoMessage}
+					<form onSubmit={this.loginOrRegister}>
+						<input
+							required
+							value={this.state.username}
+							onChange={this.onChangeUsername}
+							type="text"
+							name="username"
+							placeholder="username"
+						/>
+						<input
+							required
+							value={this.state.password}
+							onChange={this.onChangePassword}
+							type="password"
+							name="password"
+							placeholder="password"
+						/>
+						<input type="submit" value="Play" />
+					</form>
+				</aside>
+				<section id="background" />
+			</div>
+		);
+	}
 }
 
-export default Login
+export default Login;
--- project/server/routes/users.js
+++ project/server/routes/users.js
@@ -1,200 +1,172 @@
-const router = require('express').Router()
-const bcrypt = require('bcrypt')
-const User = require('../schemas/user.schema')
-const UserSession = require('../schemas/userSession.schema')
-const salt = 10
-
-
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const User = require('../schemas/user.schema');
+const UserSession = require('../schemas/userSession.schema');
+const salt = 10;
 
 ///REGISTER OR LOGIN METHOD
 router.route('/').post((req, res) => {
-    
-    const username = req.body.username
-    const password = req.body.password
-    const lastLogin = Date.now()
-    if(username.length < 5)
-    {
-        console.log(`users/ -> username.length < 5 error!`)
-        res.json({"success" : false, "message" : `Username length can't be lower than five!`})
-        return
-    }
-    if(password.length < 1)
-    {
-        console.log(`users/ -> password.length < 1 error!`)
-        res.json({"success" : false, "message" : `Password empty!`})
-        return
-    }
-    User.exists({username : username}, (err, result) => {
-        if(err)
-        {
-            console.log(`User.exists() Error: '${err}'!`)
-            res.json({"success" : false, "message" : `Server Error! '001'`})
-            return
-        }
-        if(result)
-        {
-            User.findOne({ username : username }, (err2, findOne_result) => {
-                if(err2)
-                {
-                    console.log(`User.findOne() Error: '${err2}'!`)
-                    res.json({"success" : false, "message" : `Server Error! '002'`})
-                    return
-                }
-                if(findOne_result === null)
-                {
-                    console.log(`User.findOne() returned null! username: '${username}'!`)
-                    res.json({"success" : false, "message" : `'${username}' doesn't exist!`})
-                    return
-                }
-                bcrypt.compare(password, findOne_result.password, (err3, bcrypt_result) => {
-                    if(err3)
-                    {
-                        console.log(`bcrypt.compare() Error: '${err3}'!`)
-                        res.json({"success" : false, "message" : `Server Error! '003'`})
-                        return
-                    }
-                    if(bcrypt_result)
-                    {
-                        let Session = new UserSession({
-                            userId : findOne_result._id
-                        })
+	const username = req.body.username;
+	const password = req.body.password;
+	const lastLogin = Date.now();
+	if (username.length < 5) {
+		console.log(`users/ -> username.length < 5 error!`);
+		res.json({ success: false, message: `Username length can't be lower than five!` });
+		return;
+	}
+	if (password.length < 1) {
+		console.log(`users/ -> password.length < 1 error!`);
+		res.json({ success: false, message: `Password empty!` });
+		return;
+	}
+	User.exists({ username: username }, (err, result) => {
+		if (err) {
+			console.log(`User.exists() Error: '${err}'!`);
+			res.json({ success: false, message: `Server Error! '001'` });
+			return;
+		}
+		if (result) {
+			User.findOne({ username: username }, (err2, findOne_result) => {
+				if (err2) {
+					console.log(`User.findOne() Error: '${err2}'!`);
+					res.json({ success: false, message: `Server Error! '002'` });
+					return;
+				}
+				if (findOne_result === null) {
+					console.log(`User.findOne() returned null! username: '${username}'!`);
+					res.json({ success: false, message: `'${username}' doesn't exist!` });
+					return;
+				}
+				bcrypt.compare(password, findOne_result.password, (err3, bcrypt_result) => {
+					if (err3) {
+						console.log(`bcrypt.compare() Error: '${err3}'!`);
+						res.json({ success: false, message: `Server Error! '003'` });
+						return;
+					}
+					if (bcrypt_result) {
+						let Session = new UserSession({
+							userId: findOne_result._id
+						});
 
-                        Session.save((err4, session_result) => {
-                            if(err4)
-                            {
-                                console.log(`Session.save() Error: '${err4}'!`)
-                                res.json({"success" : false, "message" : `Server Error! '004'`})
-                                return
-                            }
-                            findOne_result.updateOne({ lastlogin : lastLogin }, (err5, update_result) => {
-                                if(err5)
-                                {
-                                    console.log(`findOne_result.updateOne() Error: '${err5}'!`)
-                                    res.json({"success" : false, "message" : `Server Error! '005'`})
-                                    return
-                                }
-                                res.json({
-                                    "success" : true,
-                                    "message" : "Logined!",
-                                    "token" : session_result._id,
-                                    "username" : findOne_result.username,
-                                    "exitCode" : 0
-                                })
-                            })
-                        })
-                    }
-                    else
-                    {
-                        console.log(`bcrypt.compare() '${username}' bad pasword!:!`)
-                        res.json({"success" : false, "message" : `Wrong credentials!`})
-                        return
-                    }
-                })
-            })
-        }
-        else
-        {
-            const regdate = Date.now()
-            bcrypt.genSalt(salt,  (err6, salt_result) => {
-                if(err6)
-                {
-                    console.log(`bcrypt.genSalt() Error: '${err6}'!`)
-                    res.json({"success" : false, "message" : `Server Error! '006'`})
-                    return
-                }
-                bcrypt.hash(password, salt_result, (err7, hashedPassword) => {
-                    if(err7)
-                    {
-                        console.log(`bcrypt.hash() Error: '${err7}'!`)
-                        res.json({"success" : false, "message" : `Server Error! '007'`})
-                        return
-                    }
-                    const newUser = new User({
-                        username : username,
-                        password : hashedPassword,
-                        regdate : regdate,
-                        lastlogin : lastLogin
-                    })
-                    newUser.save((err8, save_result) =>{
-                        if(err8)
-                        {
-                            console.log(`newUser.save() Error: '${err8}'!`)
-                            res.json({"success" : false, "message" : `Server Error! '008'`})
-                            return
-                        }
-                        res.json({
-                            "success" : true,
-                            "message" : "Registered!",
-                            "username" : username,
-                            "exitCode" : 1
-                        })
-                    })
-                })
-            })
-        }
-    })
-})
+						Session.save((err4, session_result) => {
+							if (err4) {
+								console.log(`Session.save() Error: '${err4}'!`);
+								res.json({ success: false, message: `Server Error! '004'` });
+								return;
+							}
+							findOne_result.updateOne({ lastlogin: lastLogin }, (err5, update_result) => {
+								if (err5) {
+									console.log(`findOne_result.updateOne() Error: '${err5}'!`);
+									res.json({ success: false, message: `Server Error! '005'` });
+									return;
+								}
+								res.json({
+									success: true,
+									message: 'Logined!',
+									token: session_result._id,
+									username: findOne_result.username,
+									exitCode: 0
+								});
+							});
+						});
+					} else {
+						console.log(`bcrypt.compare() '${username}' bad pasword!:!`);
+						res.json({ success: false, message: `Wrong credentials!` });
+						return;
+					}
+				});
+			});
+		} else {
+			const regdate = Date.now();
+			bcrypt.genSalt(salt, (err6, salt_result) => {
+				if (err6) {
+					console.log(`bcrypt.genSalt() Error: '${err6}'!`);
+					res.json({ success: false, message: `Server Error! '006'` });
+					return;
+				}
+				bcrypt.hash(password, salt_result, (err7, hashedPassword) => {
+					if (err7) {
+						console.log(`bcrypt.hash() Error: '${err7}'!`);
+						res.json({ success: false, message: `Server Error! '007'` });
+						return;
+					}
+					const newUser = new User({
+						username: username,
+						password: hashedPassword,
+						regdate: regdate,
+						lastlogin: lastLogin
+					});
+					newUser.save((err8, save_result) => {
+						if (err8) {
+							console.log(`newUser.save() Error: '${err8}'!`);
+							res.json({ success: false, message: `Server Error! '008'` });
+							return;
+						}
+						res.json({
+							success: true,
+							message: 'Registered!',
+							username: username,
+							exitCode: 1
+						});
+					});
+				});
+			});
+		}
+	});
+});
 
 router.route('/logout').post((req, res) => {
-    const username = req.body.username
-    const token = req.body.token
+	const username = req.body.username;
+	const token = req.body.token;
 
-    User.findOne({ username : username }, (err9, logoutfindOne_result) => {
-        if(err9)
-        {
-            console.log(`User.findOne Error: '${err9}'!`)
-            res.json({"success" : false, "message" : `Server Error! '009'`})
-            return
-        }
-        if(logoutfindOne_result === null)
-        {
-            console.log(`User.findOne() returned null! username: '${username}'!`)
-            res.json({"success" : false, "message" : `'${username}' doesn't exist!`})
-            return
-        }
-        const userId = logoutfindOne_result._id
-        UserSession.findOne({ userId : userId, isDeleted : false }, (err10, usessionfindOne_result) => {
-            if (err10)
-            {
-                console.log(`UserSession.findOne Error: '${err10}'!`)
-                res.json({"success" : false, "message" : `Server Error! '010'`})
-                return
-            }
-            
-            if(usessionfindOne_result === null)
-            {
-                console.log(`UserSession.findOne() returned null! userId: '${userId}'!`)
-                res.json({"success" : false, "message" : `Session doesn't exist!`})
-                return
-            }
-            const tokenFromDb = usessionfindOne_result._id
-            if(tokenFromDb != token)
-            {
-                console.log(`Client and Server session ID mismatch: ${tokenFromDb} <> ${token}!`)
-                res.json({"success" : false, "message" : `Wrong session!`})
-                return
-            }
-            else
-            {
-                usessionfindOne_result.updateOne({
-                    isDeleted : true
-                }, (err11, usessionUpdateResult) => {
-                    if (err10)
-                    {
-                        console.log(`UserSession.updateOne Error: '${err11}'!`)
-                        res.json({"success" : false, "message" : `Server Error! '011'`})
-                        return
-                    }
-                    else 
-                    {
-                        res.json({
-                            "success" : true
-                        })
-                    }
+	User.findOne({ username: username }, (err9, logoutfindOne_result) => {
+		if (err9) {
+			console.log(`User.findOne Error: '${err9}'!`);
+			res.json({ success: false, message: `Server Error! '009'` });
+			return;
+		}
+		if (logoutfindOne_result === null) {
+			console.log(`User.findOne() returned null! username: '${username}'!`);
+			res.json({ success: false, message: `'${username}' doesn't exist!` });
+			return;
+		}
+		const userId = logoutfindOne_result._id;
+		UserSession.findOne({ userId: userId, isDeleted: false }, (err10, usessionfindOne_result) => {
+			if (err10) {
+				console.log(`UserSession.findOne Error: '${err10}'!`);
+				res.json({ success: false, message: `Server Error! '010'` });
+				return;
+			}
 
-                })
-            }
-        })
-    })
-})
-module.exports = router
+			if (usessionfindOne_result === null) {
+				console.log(`UserSession.findOne() returned null! userId: '${userId}'!`);
+				res.json({ success: false, message: `Session doesn't exist!` });
+				return;
+			}
+			const tokenFromDb = usessionfindOne_result._id;
+			if (tokenFromDb != token) {
+				console.log(`Client and Server session ID mismatch: ${tokenFromDb} <> ${token}!`);
+				res.json({ success: false, message: `Wrong session!` });
+				return;
+			} else {
+				usessionfindOne_result.updateOne(
+					{
+						isDeleted: true
+					},
+					(err11, usessionUpdateResult) => {
+						if (err10) {
+							console.log(`UserSession.updateOne Error: '${err11}'!`);
+							res.json({ success: false, message: `Server Error! '011'` });
+							return;
+						} else {
+							res.json({
+								success: true
+							});
+						}
+					}
+				);
+			}
+		});
+	});
+});
+module.exports = router;
