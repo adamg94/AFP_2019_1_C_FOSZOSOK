83a04fe6c6c97725c0f0a06d8c0e0037bec4821d
adamg94 <adamgulyas@outlook.com>
2019-12-10 02:38:43 +0100

    Palota fronted építési felület 100% kész, backend építési lehetőség 80%

--- project/server/routes/updater.js
+++ project/server/routes/updater.js
@@ -46,16 +46,15 @@ router.route("/villageupdate").post((req, res) => {
            */
           let morelLevel = village_findOne_result.buildings.temple.moral;
           GAMESETTINGS.TEMPLE_MULTIPLIER = morelLevel;
-          if(GAMESETTINGS.TEMPLE_MULTIPLIER >= 75){
+          if (GAMESETTINGS.TEMPLE_MULTIPLIER >= 75) {
             GAMESETTINGS.TEMPLE_MULTIPLIER = 1.2;
-          }
-          else if (GAMESETTINGS.TEMPLE_MULTIPLIER <= 25){
+          } else if (GAMESETTINGS.TEMPLE_MULTIPLIER <= 25) {
             GAMESETTINGS.TEMPLE_MULTIPLIER = 0.8;
-          }
-          else{
+          } else {
             GAMESETTINGS.TEMPLE_MULTIPLIER = 1;
           }
-          let maxMaterial = village_findOne_result.buildings.warehouse.level * 1000;
+          let maxMaterial =
+            village_findOne_result.buildings.warehouse.level * 1000;
           newdate = JSON.parse(body).result;
           const timeSinceLastUpdate = parseFloat(
             (Date.parse(newdate) -
@@ -68,112 +67,115 @@ router.route("/villageupdate").post((req, res) => {
             GAMESETTINGS.BASE_MULTIPLIER *
             (timeSinceLastUpdate *
               ((village_findOne_result.buildings.lumberyard.level * 80) /
-                3600)) * GAMESETTINGS.TEMPLE_MULTIPLIER;
-          
-                if(village_findOne_result.buildings.warehouse.wood > maxMaterial){
-                  village_findOne_result.buildings.warehouse.wood = maxMaterial;
-                }
+                3600)) *
+            GAMESETTINGS.TEMPLE_MULTIPLIER;
+
+          if (village_findOne_result.buildings.warehouse.wood > maxMaterial) {
+            village_findOne_result.buildings.warehouse.wood = maxMaterial;
+          }
 
           //agyag
           village_findOne_result.buildings.warehouse.brick +=
             GAMESETTINGS.BASE_MULTIPLIER *
             (timeSinceLastUpdate *
-              ((village_findOne_result.buildings.brickyard.level * 80) / 3600)) * GAMESETTINGS.TEMPLE_MULTIPLIER;
+              ((village_findOne_result.buildings.brickyard.level * 80) /
+                3600)) *
+            GAMESETTINGS.TEMPLE_MULTIPLIER;
 
-              if(village_findOne_result.buildings.warehouse.brick > maxMaterial){
-                village_findOne_result.buildings.warehouse.brick = maxMaterial;
-              }
+          if (village_findOne_result.buildings.warehouse.brick > maxMaterial) {
+            village_findOne_result.buildings.warehouse.brick = maxMaterial;
+          }
 
-              //búza
-				village_findOne_result.buildings.warehouse.wheat +=
-        GAMESETTINGS.BASE_MULTIPLIER *
-        (timeSinceLastUpdate * (3600 + village_findOne_result.buildings.mill.level * 1400) / 86400);
+          //búza
+          village_findOne_result.buildings.warehouse.wheat +=
+            GAMESETTINGS.BASE_MULTIPLIER *
+            ((timeSinceLastUpdate *
+              (3600 + village_findOne_result.buildings.mill.level * 1400)) /
+              86400);
 
-        if(village_findOne_result.buildings.warehouse.wheat >maxMaterial){
-          village_findOne_result.buildings.warehouse.wheat = maxMaterial;
-        }
+          if (village_findOne_result.buildings.warehouse.wheat > maxMaterial) {
+            village_findOne_result.buildings.warehouse.wheat = maxMaterial;
+          }
 
-      //(10000 + village_findOne_result.buildings.wheatfield.level * 200) -
+          //(10000 + village_findOne_result.buildings.wheatfield.level * 200) -
           //vas
           village_findOne_result.buildings.warehouse.iron +=
             GAMESETTINGS.BASE_MULTIPLIER *
             (timeSinceLastUpdate *
-              ((village_findOne_result.buildings.ironmine.level * 80) / 3600)) * GAMESETTINGS.TEMPLE_MULTIPLIER;
+              ((village_findOne_result.buildings.ironmine.level * 80) / 3600)) *
+            GAMESETTINGS.TEMPLE_MULTIPLIER;
 
+          if (village_findOne_result.buildings.warehouse.iron > maxMaterial) {
+            village_findOne_result.buildings.warehouse.iron = maxMaterial;
+          }
 
-              if(village_findOne_result.buildings.warehouse.iron >maxMaterial){
-                village_findOne_result.buildings.warehouse.iron = maxMaterial;
-              }
+          //templom morál
+          village_findOne_result.buildings.temple.moral -=
+            GAMESETTINGS.BASE_MULTIPLIER *
+            (timeSinceLastUpdate *
+              ((11 - village_findOne_result.buildings.temple.level) / 86400));
+          let datum = Date.parse(
+            village_findOne_result.buildings.temple.mise_ends
+          );
+          if (Date.parse(newdate) - datum > 7200000) {
+            //TODO
+          }
 
-                    //templom morál
-                    village_findOne_result.buildings.temple.moral -=
-                    GAMESETTINGS.BASE_MULTIPLIER *
-                    (timeSinceLastUpdate *
-                      ((11 - village_findOne_result.buildings.temple.level) / 86400));
-                      let datum = Date.parse(village_findOne_result.buildings.temple.mise_ends)
-                      if((Date.parse(newdate) - datum ) > 7200000){
-                        //TODO
-                      }
+          //különleges anyagok random adása
+          let d = Date.parse(
+            village_findOne_result.buildings.warehouse.last_check
+          );
+          if (Date.parse(newdate) - d > 3600000) {
+            let c_chance =
+              village_findOne_result.buildings.warehouse.copper_chance *
+              GAMESETTINGS.BASE_MULTIPLIER;
+            let s_chance =
+              village_findOne_result.buildings.warehouse.silver_chance *
+              GAMESETTINGS.BASE_MULTIPLIER;
+            let g_chance =
+              village_findOne_result.buildings.warehouse.gold_chance *
+              GAMESETTINGS.BASE_MULTIPLIER;
 
-         
+            let current_g_chance = Math.floor(Math.random() * 10);
+            let current_s_chance = Math.floor(Math.random() * 10) + 20;
+            let current_c_chance = Math.floor(Math.random() * 10) + 60;
 
-            //különleges anyagok random adása
-            let d = Date.parse(village_findOne_result.buildings.warehouse.last_check)
-            if((Date.parse(newdate)- d ) > 3600000)
-            {
-              let c_chance = village_findOne_result.buildings.warehouse.copper_chance * GAMESETTINGS.BASE_MULTIPLIER
-              let s_chance = village_findOne_result.buildings.warehouse.silver_chance * GAMESETTINGS.BASE_MULTIPLIER
-              let g_chance = village_findOne_result.buildings.warehouse.gold_chance * GAMESETTINGS.BASE_MULTIPLIER
+            const chance = Math.floor(Math.random() * 100);
 
-              let current_g_chance = Math.floor(Math.random() * 10)
-              let current_s_chance = Math.floor(Math.random() * 10) + 20
-              let current_c_chance = Math.floor(Math.random() * 10) + 60
+            //copper
 
-
-              const chance = Math.floor(Math.random() * 100)
-
-              //copper
-
-              if((c_chance + current_c_chance) >= chance)
-              {
-                //kapnak valamennyi copper-t/silver-t/goldot
-                village_findOne_result.buildings.warehouse.copper += (village_findOne_result.buildings.ironmine.level * GAMESETTINGS.BASE_MULTIPLIER)
-                village_findOne_result.buildings.warehouse.copper_chance = 0
-              }
-              else
-              {
-                village_findOne_result.buildings.warehouse.copper_chance += current_c_chance
-
-              }
-               //silver
-
-               if((s_chance + current_s_chance) >= chance)
-               {
-                village_findOne_result.buildings.warehouse.silver += (village_findOne_result.buildings.ironmine.level * GAMESETTINGS.BASE_MULTIPLIER)
-                village_findOne_result.buildings.warehouse.silver_chance = 0
-               }
-               else
-               {
-                village_findOne_result.buildings.warehouse.silver_chance += current_s_chance
-               }
-                //gold
-
-              if((g_chance + current_g_chance) >= chance)
-              {
-                village_findOne_result.buildings.warehouse.gold += (village_findOne_result.buildings.ironmine.level * GAMESETTINGS.BASE_MULTIPLIER)
-                village_findOne_result.buildings.warehouse.gold_chance = 0
-              }
-              else
-              {
-                village_findOne_result.buildings.warehouse.gold_chance += current_g_chance
-              }
-              village_findOne_result.buildings.warehouse.last_check = newdate
+            if (c_chance + current_c_chance >= chance) {
+              //kapnak valamennyi copper-t/silver-t/goldot
+              village_findOne_result.buildings.warehouse.copper +=
+                village_findOne_result.buildings.ironmine.level *
+                GAMESETTINGS.BASE_MULTIPLIER;
+              village_findOne_result.buildings.warehouse.copper_chance = 0;
+            } else {
+              village_findOne_result.buildings.warehouse.copper_chance += current_c_chance;
             }
+            //silver
 
+            if (s_chance + current_s_chance >= chance) {
+              village_findOne_result.buildings.warehouse.silver +=
+                village_findOne_result.buildings.ironmine.level *
+                GAMESETTINGS.BASE_MULTIPLIER;
+              village_findOne_result.buildings.warehouse.silver_chance = 0;
+            } else {
+              village_findOne_result.buildings.warehouse.silver_chance += current_s_chance;
+            }
+            //gold
 
-            
-         
-          
+            if (g_chance + current_g_chance >= chance) {
+              village_findOne_result.buildings.warehouse.gold +=
+                village_findOne_result.buildings.ironmine.level *
+                GAMESETTINGS.BASE_MULTIPLIER;
+              village_findOne_result.buildings.warehouse.gold_chance = 0;
+            } else {
+              village_findOne_result.buildings.warehouse.gold_chance += current_g_chance;
+            }
+            village_findOne_result.buildings.warehouse.last_check = newdate;
+          }
+
           village_findOne_result.lastupdate = newdate;
           let save_result = await village_findOne_result
             .save()
--- project/server/routes/village.js
+++ project/server/routes/village.js
@@ -119,10 +119,9 @@ router.route("/").post((req, res) => {
 
                 const newVillage = new Village({
                   userId: uid,
-                  lastupdate: newdate,
-                  
+                  lastupdate: newdate
                 });
-                newVillage.buildings.warehouse.last_check = newdate
+                newVillage.buildings.warehouse.last_check = newdate;
                 newVillage.save((err14, newVillagesave_result) => {
                   if (err14) {
                     console.log(`newVillage.save() Error: '${err14}'!`);
@@ -133,7 +132,6 @@ router.route("/").post((req, res) => {
                     return;
                   }
 
-                  
                   res.json({
                     success: true,
                     message: "Village registered!",
@@ -245,7 +243,6 @@ router.route("/miseStart").post((req, res) => {
             }
           },
           (err2, req_update_res) => {
-            
             if (err2) {
               console.log(`request.post()VillageUpdate Error: '${err2}'!`);
               res.json({ success: false, message: `Server Error! 'v002'` });
@@ -253,31 +250,387 @@ router.route("/miseStart").post((req, res) => {
             }
 
             if (req_update_res.body.success) {
+              Village.findOne(
+                { _id: req_update_res.body.villageId },
+                async (err3, foundVillage) => {
+                  if (err3) {
+                    console.log(`Village findOne error: '${err3}'!`);
+                    res.json({
+                      success: false,
+                      message: `Server Error! 'v002'`
+                    });
+                    return;
+                  }
+
+                  foundVillage.buildings.temple.mise_started =
+                    req_update_res.body.currentdatentp;
+                  foundVillage.buildings.temple.mise_ends =
+                    Date.parse(req_update_res.body.currentdatentp) + 7200000;
+                  let save_result = await foundVillage
+                    .save()
+                    .then(_ => {
+                      res.json({
+                        currentdatentp:
+                          foundVillage.buildings.temple.mise_started,
+                        success: true,
+                        message: "Village Updated",
+                        village: foundVillage
+                      });
+                      console.log("OK");
+                      return;
+                    })
+                    .catch(err => console.log(err));
+                }
+              );
+            } else {
+              res.json({
+                success: false,
+                message: `You don't have a village O.o! ` //ennek soha nem kéne lefutnia.
+              });
+              return;
+            }
+          }
+        );
+      } else {
+        res.json({
+          success: false,
+          message: `You have no access to this operation!`
+        });
+      }
+    }
+  );
+});
+
+router.route("/palacenormalupgrade").post((req, res) => {
+  const token = req.body.token;
+  const username = req.body.username;
+  const id = req.body.buildingId;
+  request.post(
+    "http://localhost:5000/users/verify",
+    {
+      json: {
+        token: token,
+        username: username
+      }
+    },
+    (err7, req_res) => {
+      if (err7) {
+        console.log(`request.post() Error: '${err7}'!`);
+        res.json({ success: false, message: `Server Error! 'v007'` });
+        return;
+      }
+
+      if (req_res.body.success) {
+        const uid = req_res.body.uid;
+
+        request.post(
+          "http://localhost:5000/update/villageupdate",
+          {
+            json: {
+              userId: uid
+            }
+          },
+          (err8, req_update_res) => {
+            if (err8) {
+              console.log(`request.post()VillageUpdate Error: '${err8}'!`);
+              res.json({ success: false, message: `Server Error! 'v008'` });
+              return;
+            }
+
+            if (req_update_res.body.success) {
+              Village.findById(
+                { _id: req_update_res.body.villageId },
+                async (err9, findByResult) => {
+                  if (err9) {
+                    console.log(`Village.findById Error: '${err9}'!`);
+                    res.json({
+                      success: false,
+                      message: `Server Error! 'v009'`
+                    });
+                    return;
+                  }
+                  let building;
+                  switch (parseInt(id)) {
+                    case 1:
+                      building = findByResult.buildings.palace;
+                      break;
+                    case 2:
+                      building = findByResult.buildings.merch;
+                      break;
+                    case 3:
+                      building = findByResult.buildings.merchworkshop;
+                      break;
+                    case 4:
+                      building = findByResult.buildings.temple;
+                      break;
+                    case 5:
+                      building = findByResult.buildings.wall;
+                      break;
+                    case 6:
+                      building = findByResult.buildings.warehouse;
+                      break;
+                    case 7:
+                      building = findByResult.buildings.hideout;
+                      break;
+                    case 8:
+                      building = findByResult.buildings.statue;
+                      break;
+                    case 9:
+                      building = findByResult.buildings.metalfurnace;
+                      break;
+                    case 10:
+                      building = findByResult.buildings.mill;
+                      break;
+                    case 11:
+                      building = findByResult.buildings.wheatfield;
+                      break;
+                    case 12:
+                      building = findByResult.buildings.ironmine;
+                      break;
+                    case 13:
+                      building = findByResult.buildings.brickyard;
+                      break;
+                    case 14:
+                      building = findByResult.buildings.lumberyard;
+                      break;
+                    default:
+                      break;
+                  }
+
+                  let constructionTime;
+                  let constructionPrice;
+                  if(id == 1) {
+                    if (building.level > 0 && building.level <= 20) {
+                      constructionTime =
+                        building.level * 5
+                      constructionPrice = building.level * 50;
+                    } else if (building.level > 20 && building.level <= 40) {
+                      constructionTime =
+                        building.level * 10
+                      constructionPrice = building.level * 500;
+                    } else {
+                      constructionTime =
+                        building.level * 20
+                      constructionPrice = building.level * 5000;
+                    }
+                  }
+                  else{
+                  if (building.maxlevel == 50) {
+                    if (building.level > 0 && building.level <= 20) {
+                      constructionTime =
+                        building.level * 5 -
+                        building.level *
+                          5 *
+                          (findByResult.buildings.palace.level / 100);
+                      constructionPrice = building.level * 50;
+                    } else if (building.level > 20 && building.level <= 40) {
+                      constructionTime =
+                        building.level * 10 -
+                        building.level *
+                          10 *
+                          (findByResult.buildings.palace.level / 100);
+                      constructionPrice = building.level * 500;
+                    } else {
+                      constructionTime =
+                        building.level * 20 -
+                        building.level *
+                          20 *
+                          (findByResult.buildings.palace.level / 100);
+                      constructionPrice = building.level * 5000;
+                    }
+                  }
+                  else if(building.maxlevel == 30)
+                  {
+                    if (building.level > 0 && building.level <= 10) {
+                      constructionTime =
+                        building.level * 5 -
+                        building.level *
+                          5 *
+                          (findByResult.buildings.palace.level / 100);
+                      constructionPrice = building.level * 100;
+                    } else if (building.level > 10 && building.level <= 20) {
+                      constructionTime =
+                        building.level * 10 -
+                        building.level *
+                          10 *
+                          (findByResult.buildings.palace.level / 100);
+                      constructionPrice = building.level * 1000;
+                    } else {
+                      constructionTime =
+                        building.level * 20 -
+                        building.level *
+                          20 *
+                          (findByResult.buildings.palace.level / 100);
+                      constructionPrice = building.level * 10000;
+                    }
+                  }
+                  else if(building.maxlevel == 10)
+                  {
+                    if (building.level > 0 && building.level <= 3) {
+                      constructionTime =
+                        building.level * 5 -
+                        building.level *
+                          5 *
+                          (findByResult.buildings.palace.level / 100);
+                      constructionPrice = building.level * 2500;
+                    } else if (building.level > 3 && building.level <= 6) {
+                      constructionTime =
+                        building.level * 10 -
+                        building.level *
+                          10 *
+                          (findByResult.buildings.palace.level / 100);
+                      constructionPrice = building.level * 5000;
+                    } else {
+                      constructionTime =
+                        building.level * 20 -
+                        building.level *
+                          20 *
+                          (findByResult.buildings.palace.level / 100);
+                      constructionPrice = building.level * 10000;
+                    }
+                    
+                  }
+                  else if(building.maxlevel == 1)
+                  {
+                    constructionTime = 4000 * (findByResult.buildings.palace.level / 100)
+                    constructionPrice = 500000
+                    
+                  }
+                }
+                  
+                let lumber = findByResult.buildings.warehouse.wood
+                let brick = findByResult.buildings.warehouse.brick
+                let iron = findByResult.buildings.warehouse.iron
+                let wheat = findByResult.buildings.warehouse.wheat
+               
+                if((lumber - constructionPrice) >= 0
+                  && (brick - constructionPrice) >= 0
+                  && (iron - constructionPrice) >= 0
+                  && (wheat - (constructionPrice/10)) >=0)
+                {
+                  
+
+
+
+
+                  findByResult.buildings.warehouse.wood -= constructionPrice
+                  findByResult.buildings.warehouse.brick -= constructionPrice
+                  findByResult.buildings.warehouse.iron -= constructionPrice
+                  findByResult.buildings.warehouse.wheat -= constructionPrice/10
+
+                  let save_result = await findByResult
+                  .save()
+                  .then(_ => {
+                    res.json({
+                      success: true,
+                      message: "Building started!",
+                      village: findByResult,
+                      currentdatentp: newdate
+                    });
+
+
+
+
+                  //csatlakozni a construction táblához és beleirni a dolgokat.
+
+
+
+
+
+
+
+
+                    return;
+                  })
+                  .catch(err => console.log(err));
+
+
+
+
+
+                }
+                else
+                {
+                  res.json({
+                    success: false,
+                    message: "Not enough resource"
+                  });
+                  console.log("Price/resource: " + constructionPrice);
+                  console.log("Time(minutes): " + constructionTime);
+                }
+
+
+                
+               
+
+
+    
+                }
+              );
+
               
-            Village.findOne({_id : req_update_res.body.villageId}, async (err3, foundVillage) => {
-              if (err3) {
-                console.log(`Village findOne error: '${err3}'!`);
-                res.json({ success: false, message: `Server Error! 'v002'` });
-                return;
-              }
-              
-              foundVillage.buildings.temple.mise_started = req_update_res.body.currentdatentp;
-              foundVillage.buildings.temple.mise_ends = Date.parse(req_update_res.body.currentdatentp) + 
-              7200000; 
-              let save_result = await foundVillage
-              .save()
-              .then(_ => {
-                res.json({
-                  currentdatentp: foundVillage.buildings.temple.mise_started,
-                  success: true,
-                  message: "Village Updated",
-                  village: foundVillage
-                });
-                console.log('OK');
-                return;
-              })
-              .catch(err => console.log(err));              
-            })
+            } else {
+              res.json({
+                success: false,
+                message: `You don't have a village O.o! ` //ennek soha nem kéne lefutnia.
+              });
+              return;
+            }
+          }
+        );
+      } else {
+        res.json({
+          success: false,
+          message: `You have no access to this operation!`
+        });
+      }
+    }
+  );
+});
+router.route("/palacerareupgrade").post((req, res) => {
+  const token = req.body.token;
+  const username = req.body.username;
+  const id = req.body.buildingId;
+  request.post(
+    "http://localhost:5000/users/verify",
+    {
+      json: {
+        token: token,
+        username: username
+      }
+    },
+    (err7, req_res) => {
+      if (err7) {
+        console.log(`request.post() Error: '${err7}'!`);
+        res.json({ success: false, message: `Server Error! 'v007'` });
+        return;
+      }
+
+      if (req_res.body.success) {
+        const uid = req_res.body.uid;
+
+        request.post(
+          "http://localhost:5000/update/villageupdate",
+          {
+            json: {
+              userId: uid
+            }
+          },
+          (err2, req_update_res) => {
+            if (err2) {
+              console.log(`request.post()VillageUpdate Error: '${err2}'!`);
+              res.json({ success: false, message: `Server Error! 'v002'` });
+              return;
+            }
+
+            if (req_update_res.body.success) {
+              ////////////
+              console.log("rare");
+              console.log(req.body);
+              res.json({
+                success: true,
+                message: "REQUEST RARE"
+              });
             } else {
               res.json({
                 success: false,
